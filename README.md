08c36b974886f0aca81a7c452a1650e21e04429a
Tony Mamedbekov <tony.mamedbekov@gmail.com>
2018-10-16 21:04:41 -0500

    update

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-# Consultant-list
+1) Open a command prompt in the project's root directory
 
-description coming soon.
+2) Type: `npm install`
+    This installs the dependencies as defined in the package.json file.
+
+3) Type: `npm start`
+    This launches the TypeScript compiler (tsc) to compile the application and wait for changes.
+    It also starts the lite-server and launches the browser to run the application.
---
+++ app/app.component.js
@@ -0,0 +1,66 @@
+System.register(['angular2/core', 'angular2/http', 'rxjs/Rx', 'angular2/router', './consultants/consultant-list.component', './consultants/consultant.service', './home/welcome.component', './consultants/consultant-detail.component'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+        var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+        if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+        else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+        return c > 3 && r && Object.defineProperty(target, key, r), r;
+    };
+    var __metadata = (this && this.__metadata) || function (k, v) {
+        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+    };
+    var core_1, http_1, router_1, consultant_list_component_1, consultant_service_1, welcome_component_1, consultant_detail_component_1;
+    var AppComponent;
+    return {
+        setters:[
+            function (core_1_1) {
+                core_1 = core_1_1;
+            },
+            function (http_1_1) {
+                http_1 = http_1_1;
+            },
+            function (_1) {},
+            function (router_1_1) {
+                router_1 = router_1_1;
+            },
+            function (consultant_list_component_1_1) {
+                consultant_list_component_1 = consultant_list_component_1_1;
+            },
+            function (consultant_service_1_1) {
+                consultant_service_1 = consultant_service_1_1;
+            },
+            function (welcome_component_1_1) {
+                welcome_component_1 = welcome_component_1_1;
+            },
+            function (consultant_detail_component_1_1) {
+                consultant_detail_component_1 = consultant_detail_component_1_1;
+            }],
+        execute: function() {
+            AppComponent = (function () {
+                function AppComponent() {
+                    this.pageTitle = 'MVP';
+                }
+                AppComponent = __decorate([
+                    core_1.Component({
+                        selector: 'consultant-app',
+                        template: "\n    <div>\n        <nav class='navbar navbar-default'>\n            <div class='container-fluid'>\n            <a class='navbar-brand'>{{pageTitle}}</a>\n            <ul class='nav navbar-nav'>\n                <li><a [routerLink]=\"['Home']\">Home</a></li>\n                <li><a [routerLink]=\"['Consultants']\">Consultants</a></li>\n            </ul>\n            </div>\n        </nav>\n        <div class='container'>\n        <router-outlet></router-outlet>\n        </div>\n    </div>\n    ",
+                        directives: [router_1.ROUTER_DIRECTIVES],
+                        providers: [consultant_service_1.ConsultantService,
+                            http_1.HTTP_PROVIDERS,
+                            router_1.ROUTER_PROVIDERS]
+                    }),
+                    router_1.RouteConfig([
+                        { path: '/home', name: 'Home', component: welcome_component_1.WelcomeComponent, useAsDefault: true },
+                        { path: '/consultants', name: 'Consultants', component: consultant_list_component_1.ConsultantListComponent },
+                        { path: '/consultant/:id', name: 'ConsultantDetail', component: consultant_detail_component_1.ConsultantDetailComponent }
+                    ]), 
+                    __metadata('design:paramtypes', [])
+                ], AppComponent);
+                return AppComponent;
+            }());
+            exports_1("AppComponent", AppComponent);
+        }
+    }
+});
+//# sourceMappingURL=app.component.js.map
